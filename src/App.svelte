df2c758b2cf169f60dad88e921100aa28ead72f0
Robin <robin23jac@gmail.com>
2020-10-04 19:24:06 +0200

    feat: Add capacitor
    Added android and ios enviroments. Changed svelte example to capacitor
    svelte example, shown [here](https://capacitorjs.com/solution/svelte#install)

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	export let name: string;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { Plugins } from '@capacitor/core';
+  
+	let loc;
+	async function getCurrentPosition(){
+	  const { Geolocation } = Plugins;
+	  const res = await Geolocation.getCurrentPosition()
+	  loc = res
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </script>
+  
+  <div>
+	<h1>Geolocation</h1>
+	<p>Your location is:</p>
+	<p>Latitude: {loc?.coords.latitude}</p>
+	<p>Longitude: {loc?.coords.longitude}</p>
+  
+	<button on:click={getCurrentPosition}>
+	  Get Current Location
+	</button>
+  </div>
